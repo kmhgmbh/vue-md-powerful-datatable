2f8ae4f63ccb1d1569829a2c38b72f2aae7b6013
Mark Labenski <mark.labenski@gmail.com>
2018-01-02 17:09:04 +0100

    refactor: change dev js file for testing with proper mdl css

--- src/dev.js
+++ src/dev.js
@@ -1,5 +1,18 @@
 import Vue from 'vue';
+import VueMdl from 'vue-mdl';
+import VueMaterial from 'vue-material';
+
 import Main from './main';
+
+require('vue-material/dist/vue-material.css');
+
+Vue.use(VueMaterial);
+Vue.material.registerTheme('blue', {});
+
+require('material-design-lite/material.min.js');
+require('material-design-lite/material.min.css');
+
+Vue.use(VueMdl);
 
 /* eslint-disable no-new */
 new Vue({
